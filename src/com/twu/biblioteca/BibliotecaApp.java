8d9308d3a9dc3fc33ccc2fabe4842022b63b0676
wyb1995 <wy_b1995@163.com>
2017-07-24 11:17:31 +0800

    mainMenu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -27,25 +27,26 @@ public class BibliotecaApp {
         String[] bookList = bookList();
         System.out.println("Main Menu");
         String[] mainMenu = mainMenu();
-        for(int i = 0; i < mainMenu.length; i++) {
-            System.out.println(i+1+"  "+mainMenu[i]);
+        for (int i = 0; i < mainMenu.length; i++) {
+            System.out.println(i + 1 + "  " + mainMenu[i]);
         }
         System.out.println("Please enter You choose Number: ");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        if(number > mainMenu.length || number < 1){
+        while (number > mainMenu.length || number < 1) {
             System.out.println("input error");
-        }else{
-            switch(number){
-                case 1:
-                    System.out.println("bookList: ");
-                    System.out.println("书名      作者      出版日期");
-                    for (String book : bookList) {
-                        System.out.println(book);
-                    }
-                    break;
-            }
-
+            System.out.println("Please input again: ");
+            sc = new Scanner(System.in);
+            number = sc.nextInt();
+        }
+        switch (number) {
+            case 1:
+                System.out.println("bookList: ");
+                System.out.println("书名      作者      出版日期");
+                for (String book : bookList) {
+                    System.out.println(book);
+                }
+                break;
         }
     }
 }
