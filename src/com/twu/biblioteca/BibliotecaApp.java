fb6627afdad85def4eb3b5160e7cdcea182af28c
wyb1995 <wy_b1995@163.com>
2017-07-24 11:28:09 +0800

    add quit function

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -17,36 +17,57 @@ public class BibliotecaApp {
     }
 
     public static String[] mainMenu() {
-        String[] mainMenu = {"BookList"};
+        String[] mainMenu = {"BookList", "Quit"};
         return mainMenu;
     }
 
-    public static void main(String[] args) {
-        String welcomeMessage = welcomeMessage();
-        System.out.println(welcomeMessage + '\n');
-        String[] bookList = bookList();
+    public static void showMainMenu() {
         System.out.println("Main Menu");
         String[] mainMenu = mainMenu();
         for (int i = 0; i < mainMenu.length; i++) {
             System.out.println(i + 1 + "  " + mainMenu[i]);
         }
+    }
+
+    public static Integer chooseNumber() {
         System.out.println("Please enter You choose Number: ");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
+        String[] mainMenu = mainMenu();
         while (number > mainMenu.length || number < 1) {
             System.out.println("input error");
             System.out.println("Please input again: ");
             sc = new Scanner(System.in);
             number = sc.nextInt();
         }
-        switch (number) {
-            case 1:
-                System.out.println("bookList: ");
-                System.out.println("书名      作者      出版日期");
-                for (String book : bookList) {
-                    System.out.println(book);
-                }
-                break;
+        return number;
+    }
+
+    public static void main(String[] args) {
+        String welcomeMessage = welcomeMessage();
+        System.out.println(welcomeMessage + '\n');
+        String[] bookList = bookList();
+        showMainMenu();
+        int number = chooseNumber();
+        boolean flag = true;
+        while (flag) {
+            switch (number) {
+                case 1:
+                    System.out.println("bookList: ");
+                    System.out.println("书名      作者      出版日期");
+                    for (String book : bookList) {
+                        System.out.println(book);
+                    }
+                    break;
+                case 2:
+                    System.out.println("thanks");
+                    flag = false;
+                    break;
+            }
+            if (flag) {
+                showMainMenu();
+                number = chooseNumber();
+            }
         }
     }
 }
