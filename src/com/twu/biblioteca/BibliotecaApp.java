0946ebf76893be96341aeaf8815031409c17171b
wyb1995 <wy_b1995@163.com>
2017-07-24 12:21:50 +0800

    finish

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,22 +3,56 @@ package com.twu.biblioteca;
 import java.util.Scanner;
 
 public class BibliotecaApp {
+    static Book book1 = new Book("javaScript", "yuanbo", "2017.7.1");
+    static Book book2 = new Book("java", "yuanbo", "2017.7.1");
+    static Book book3 = new Book("ruby", "yuanbo", "2017.7.1");
 
     public static String welcomeMessage() {
         return "Welcome to Biblioteca!";
     }
 
-    public static String[] bookList() {
-        Book book1 = new Book("javaScript", "yuanbo", "2017.7.1");
-        Book book2 = new Book("javaScript", "yuanbo", "2017.7.1");
-        Book book3 = new Book("javaScript", "yuanbo", "2017.7.1");
-        String[] bookList = {book1.getBook(), book2.getBook(), book3.getBook()};
+    public static Book[] bookList() {
+        Book[] bookList = {book1, book2, book3};
+        System.out.println("bookList: ");
+        System.out.println("书名      作者      出版日期");
+        for (Book book : bookList) {
+            if (!book.getChecked()) {
+                System.out.println(book.getBook());
+            }
+        }
         return bookList;
     }
 
     public static String[] mainMenu() {
-        String[] mainMenu = {"BookList", "Quit"};
+        String[] mainMenu = {"BookList", "check out book", "return book", "Quit"};
         return mainMenu;
+    }
+
+    public static void checkOutBook() {
+        System.out.println("please input book Number: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        Book[] bookList = bookList();
+        if (number > bookList.length || number <= 0) {
+            System.out.println("That book is not available.");
+        } else {
+            bookList[number - 1].setChecked(true);
+            System.out.println("Thank you! Enjoy the book");
+        }
+    }
+
+    public static boolean returnBook() {
+        System.out.println("please input book Name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        Book[] bookList = bookList();
+        for (Book book : bookList) {
+            if (book.getName().equals(name) && book.getChecked()) {
+                book.setChecked(false);
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void showMainMenu() {
@@ -46,20 +80,26 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         String welcomeMessage = welcomeMessage();
         System.out.println(welcomeMessage + '\n');
-        String[] bookList = bookList();
         showMainMenu();
         int number = chooseNumber();
         boolean flag = true;
         while (flag) {
             switch (number) {
                 case 1:
-                    System.out.println("bookList: ");
-                    System.out.println("书名      作者      出版日期");
-                    for (String book : bookList) {
-                        System.out.println(book);
-                    }
+                    Book[] bookList = bookList();
                     break;
                 case 2:
+                    checkOutBook();
+                    break;
+                case 3:
+                    boolean isReturn = returnBook();
+                    if(isReturn){
+                        System.out.println("Thank you for returning the book.");
+                    }else{
+                        System.out.println("That is not a valid book to return.");
+                    }
+                    break;
+                case 4:
                     System.out.println("thanks");
                     flag = false;
                     break;
--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -2,11 +2,25 @@ package com.twu.biblioteca;
 
 public class Book {
     String name, auth, date;
+    boolean isChecked;
 
     public Book(String name, String auth, String date){
         this.name = name;
         this.auth = auth;
         this.date = date;
+        this.isChecked = false;
+    }
+
+    public void setChecked(boolean isChecked){
+        this.isChecked = isChecked;
+    }
+
+    public boolean getChecked(){
+        return this.isChecked;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public String getBook(){
